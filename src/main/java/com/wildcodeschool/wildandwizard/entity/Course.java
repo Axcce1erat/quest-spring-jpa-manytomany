fe78f3e0ffc937ff7145ba59bc71b1af2d0c679f
Axcce1erat <axelschwarz@gmx.de>
2020-01-21 15:25:24 +0100

    Final

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
@@ -1,9 +1,9 @@
 package com.wildcodeschool.wildandwizard.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 @Entity
 public class Course {
@@ -12,6 +12,20 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @ManyToMany(mappedBy = "courses",
+            fetch = FetchType.EAGER,
+            cascade = CascadeType.ALL
+    )
+    private List<Wizard> wizards = new ArrayList<Wizard>();
+
+    public List<Wizard> getWizards(){
+        return wizards;
+    }
+
+    public void setWizards(List<Wizard> wizards){
+        this.wizards = wizards;
+    }
 
     public Course() {
     }
